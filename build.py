90d5393a83d931ad8ad106c95fb6a5dfe8c089c4
David Alves De Araujo <daviaraujo@ml.com>
2026-06-03 09:54:44 -0300

    fix: Jun/26 Aba 1 populado com dados BT_PRODUCT_ORDERS (1-2/jun)

--- build.py
+++ build.py
@@ -615,10 +615,10 @@ const RAW = [
   {mes:"Mai/26",nivel:"Especialista",    resellers:63,   pedidos:560,  ativos:108,  tpv_m0:2098419,  tpv_m1:4649704,  tpv_total:17605917,  pedidos_pro:498,  pedidos_smart:62,  ativos_pro:95,   ativos_smart:13},
   {mes:"Mai/26",nivel:"Empreendedor",    resellers:45,   pedidos:745,  ativos:156,  tpv_m0:3862445,  tpv_m1:5560670,  tpv_total:24301846,  pedidos_pro:588,  pedidos_smart:157, ativos_pro:81,   ativos_smart:75},
   {mes:"Mai/26",nivel:"Top Empreendedor",resellers:36,   pedidos:1218, ativos:279,  tpv_m0:5235329,  tpv_m1:13139882, tpv_total:52755517,  pedidos_pro:987,  pedidos_smart:231, ativos_pro:232,  ativos_smart:47},
-  {mes:"Jun/26",nivel:"Aprendiz",        resellers:0,  pedidos:0, ativos:0, tpv_m0:0,  tpv_m1:0,  tpv_total:0,  pedidos_pro:0,  pedidos_smart:0, ativos_pro:0,   ativos_smart:0},
-  {mes:"Jun/26",nivel:"Especialista",        resellers:0,  pedidos:0, ativos:0, tpv_m0:0,  tpv_m1:0,  tpv_total:0,  pedidos_pro:0,  pedidos_smart:0, ativos_pro:0,   ativos_smart:0},
-  {mes:"Jun/26",nivel:"Empreendedor",        resellers:0,  pedidos:0, ativos:0, tpv_m0:0,  tpv_m1:0,  tpv_total:0,  pedidos_pro:0,  pedidos_smart:0, ativos_pro:0,   ativos_smart:0},
-  {mes:"Jun/26",nivel:"Top Empreendedor",        resellers:0,  pedidos:0, ativos:0, tpv_m0:0,  tpv_m1:0,  tpv_total:0,  pedidos_pro:0,  pedidos_smart:0, ativos_pro:0,   ativos_smart:0},
+  {mes:"Jun/26",nivel:"Aprendiz",        resellers:128, pedidos:242,  ativos:0, tpv_m0:0, tpv_m1:null, tpv_total:0, pedidos_pro:220, pedidos_smart:22, ativos_pro:0, ativos_smart:0},
+  {mes:"Jun/26",nivel:"Especialista",    resellers:18,  pedidos:117,  ativos:0, tpv_m0:0, tpv_m1:null, tpv_total:0, pedidos_pro:104, pedidos_smart:13, ativos_pro:0, ativos_smart:0},
+  {mes:"Jun/26",nivel:"Empreendedor",    resellers:12,  pedidos:90,   ativos:0, tpv_m0:0, tpv_m1:null, tpv_total:0, pedidos_pro:71,  pedidos_smart:19, ativos_pro:0, ativos_smart:0},
+  {mes:"Jun/26",nivel:"Top Empreendedor",resellers:15,  pedidos:218,  ativos:0, tpv_m0:0, tpv_m1:null, tpv_total:0, pedidos_pro:177, pedidos_smart:41, ativos_pro:0, ativos_smart:0},
 ];
 
 const DAILY = """ + daily_json + """;
